01f1601e7bea3f2d621e27b373eeaaf5bc0edbb0
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-07-06 07:33:25 +0530

    class enum moved to class module from subject module

--- src/student/subject.rs
+++ src/student/subject.rs
@@ -3,10 +3,3 @@ use std::collections::HashMap;
 pub type Marks = u16;
 pub type SubjectName = String;
 pub type Subjects = HashMap<SubjectName, Marks>;
-
-#[derive(Debug, Eq, PartialEq)]
-pub enum Class{
-    Distinction,
-    FirstClass,
-    SecondClass,
-}
