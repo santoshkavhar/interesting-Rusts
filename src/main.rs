dd96a75d9bcbed21a1b259a3862a7c82c2bd4172
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-08-15 22:37:02 +0530

    error handling added in main to exit without panicing.

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,25 @@ mod input;
 mod sorting;
 mod student;
 use crate::sorting::Sort;
+use std::process;
 
 // TODO: Better Error handling
 fn main() {
-    let no_of_students = student::Student::get_no_of_students();
+    let no_of_students = student::Student::get_no_of_students().unwrap_or_else(|err| {
+        // TODO: Use eprintln!
+        println!("Problem getting number of students: {}", err);
+        process::exit(1);
+    });
 
-    let mut students = student::Student::create_students(no_of_students);
+    let mut students = student::Student::create_students(no_of_students).unwrap_or_else(|err| {
+        // TODO: Use eprintln!
+        println!("Problem creating students: {}", err);
+        process::exit(1);
+    });
 
-    //println!("{:?}", students);
-
-    students.sorting_menu();
+    if let Err(e) = students.sorting_menu() {
+        // TODO: Use eprintln!
+        println!("Sorting Menu Error! {}", e);
+        process::exit(1);
+    };
 }
