6f476637a09be7caf63933da60e4e212abde17df
Santosh Kavhar <santosh.kavhar@joshsoftware.com>
2021-09-07 10:24:07 +0530

    client program added for chat app

--- chat_app_client/src/main.rs
+++ chat_app_client/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpStream;
+use std::io::{self, BufReader, BufRead, Write};
+
+const LOCAL:&str = "0.0.0.0:3333";
+
+fn main(){
+    let mut stream = TcpStream::connect(LOCAL).expect("Couldn't connect!");
+    loop{
+        let mut input : String = String::new();
+        let mut buf: Vec<u8> = Vec::new();
+        io::stdin().read_line(&mut input).expect("Couldn't read!");
+
+        stream.write(input.as_bytes()).expect("Failed to write!");
+
+        let mut reader = BufReader::new(&stream);
+
+        reader.read_until(b'\n', &mut buf).expect("Failed to read!");
+
+        println!("From Server: {}", std::str::from_utf8(&buf).expect("Couldn't Write to buffer as string") );
+    }    
 }
